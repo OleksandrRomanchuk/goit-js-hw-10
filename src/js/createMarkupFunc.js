c3730643bd1bd51c2fa188dc1b75f6964623eda7
Oleksandr Romanchuk <novator0511@gmail.com>
2022-11-06 17:35:09 +0200

    Update createMarkupFunc.js

--- src/js/createMarkupFunc.js
+++ src/js/createMarkupFunc.js
@@ -4,10 +4,11 @@ function createListItemsMarkup(data) {
       ({
         name: { official },
         flags: { svg },
-      }) => `<li class="country-list__item">
-  <img src="${svg}" alt="${official} flag" class="country-list__flag">
-  <p class="country-list__name">${official}</p>
-</li>`
+      }) => `<li class="country-list__item" style="background-image:url(${svg})">
+                <a class="country-list__info-wrapper" href="#">
+                  <p class="country-list__name">${official}</p>
+                </a>
+            </li>`
     )
     .join('');
 }
@@ -16,26 +17,29 @@ function createFinalResultCardMarkup(data) {
   return data
     .map(
       ({
-        name: { official },
+        name: { common, official },
         flags: { svg },
         population,
         capital,
         languages,
-      }) => `<div class="country__final-result">
-  <img src="${svg}" alt="${official} flag" class="country__flag">
-  <p class="country__name">${official}</p>
-  <ul class="country-info">
-    <li class="country-info__item">
-      <p class="country-info__capital">Capital:<span> ${capital}</span></p>
-    </li>
-    <li class="country-info__item">
-      <p class="country-info__population">Population:<span> ${population}</span></p>
-    </li>
-    <li class="country-info__item">
-      <p class="country-info__languages">Languages:<span> ${languages}</span></p>
-    </li>
-  </ul>
-</div>`
+      }) => `<img class="country__final-result" src="${svg}">
+              <div class="country-info__wrapper">
+                <p class="country-info__name">${common}</p>
+                <ul class="country-info__categories">
+                  <li class="country-info__item">
+                    <p class="country-info__category">Official name:<span class="country-info__category--value"> ${official}</span></p>
+                  </li>
+                  <li class="country-info__item">
+                    <p class="country-info__category">Capital:<span class="country-info__category--value"> ${capital}</span></p>
+                  </li>
+                  <li class="country-info__item">
+                    <p class="country-info__category">Population:<span class="country-info__category--value"> ${population}</span></p>
+                  </li>
+                  <li class="country-info__item">
+                    <p class="country-info__category">Languages:<span class="country-info__category--value"> ${languages}</span></p>
+                  </li>
+                </ul>
+              </div>`
     )
     .join('');
 }
